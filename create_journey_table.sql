ce65a3d7d13c1765987422646a802af8af4a8f04
RamneekM-optimus <53003061+RamneekM-optimus@users.noreply.github.com>
2019-07-29 17:37:44 +0530

    Delete create_journey_table.sql

--- create_journey_table.sql
+++
@@ -1,1 +0,0 @@
-create table journey_details(train_id int foreign key REFERENCES train_details(train_id),station_id int foreign key REFERENCES station_details(station_id),distance int,depature datetime );
